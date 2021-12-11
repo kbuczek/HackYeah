f27d2b88877ba19ae54edb7f2b579d4e7988ff9f
rsludwikowski <rsludwikowski@gmail.com>
2021-12-11 01:54:01 +0100

    MYSql update

--- screens/about.js
+++ screens/about.js
@@ -1,14 +1,47 @@
 import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import { Button, StyleSheet, View, Text, Image } from "react-native";
 import { globalStyles } from "../styles/global";
+
+/*var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "http://149.156.115.209/phpmyadmin",
+  user: "s299918",
+  password: "szott-ludwikowskirobert",
+});
+
+con.connect(function (err) {
+  if (err) {
+    alert("ZJEBALO SIE");
+  } else {
+    alert("CONNECTED");
+  }
+});*/
+
+function BUTT_PRESS() {
+  alert("TY KUURrwo22");
+}
 
 export default function About() {
   return (
     <View style={globalStyles.container}>
-      <Text>About Screen</Text>
-      <Image source={require("../assets/working.jpg")} />
+      <Text>About KUTAS 22</Text>
+      <View style={styles.IMG}>
+        <Image source={require("../assets/working.jpg")} />
+      </View>
+      <Button
+        title="KUTAS KOZŁA"
+        onPress={BUTT_PRESS}
+        style={styles.button}
+        color="blue"
+        marginTop={10}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  IMG: {
+    paddingBottom: 100,
+  },
+});
